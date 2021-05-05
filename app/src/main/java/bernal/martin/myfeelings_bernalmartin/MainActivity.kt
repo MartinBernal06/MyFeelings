a0de7ceac8d55c8abea96efa309f34444607f4d9
MartinBernal06 <78180863+MartinBernal06@users.noreply.github.com>
2021-05-04 21:02:50 -0700

    version inicial

---
+++ app/src/main/java/bernal/martin/myfeelings_bernalmartin/MainActivity.kt
@@ -0,0 +1,11 @@
+package bernal.martin.myfeelings_bernalmartin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
